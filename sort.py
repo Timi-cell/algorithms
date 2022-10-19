a5a5a8bc4f6608c0c385dd09d640df5b9ec3b4ee
Timi-cell <adedayotimilehin10@gmail.com>
2022-10-18 19:33:16 -0700

    added comments

--- sort.py
+++ sort.py
@@ -1,6 +1,10 @@
+# a function to get the index of the smallest number
 def find_smallest(arr):
+    # save the first number as the smallest
     smallest = arr[0]
+    # save the index as 0
     smallest_index = 0
+    # run a loop to check the smallest number
     for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
@@ -8,10 +12,13 @@ def find_smallest(arr):
     return smallest_index
 
 
+# function to sort the array passed
 def selection_sort(arr):
     new_arr = []
     for i in range(len(arr)):
+        # get the index of the smallest number
         smallest = find_smallest(arr)
+        # append the smallest number to the new array after removing it from the old array
         new_arr.append(arr.pop(smallest))
     return new_arr
 
